8a621b02640d7e769cc624ee3bfe431198b521cb
RiteshChepuri <riteshchepuri0@gmail.com>
2025-08-05 13:37:28 +0530

    feat: added stow and dotfiles setup in scripts

--- 2.sh
+++ 2.sh
@@ -2,6 +2,26 @@
 
 yay -S wezterm zen-browser-bin bitwarden ghostty --noconfirm
 
-yay -S lsd waybar deno blueberry nvim stow fd ripgrep fzf yazi starship zoxide lazygit tmux git-delta dunst github-cli tmux sesh-bin --noconfirm
+yay -S bat lsd waybar deno blueberry nvim stow fd ripgrep fzf yazi starship zoxide lazygit tmux git-delta dunst github-cli sesh-bin --noconfirm
 
-yay -S bat lua51-jsregexp cargo ruby composer php nodejs npm python-pip julia imagemagick jdk-openjdk --noconfirm
+yay -S lua51-jsregexp cargo ruby composer php nodejs npm python-pip julia imagemagick jdk-openjdk --noconfirm
+
+git clone https://github.com/RiteshChepuri/dotfiles.git
+
+rm -rf ~/.config/fish
+
+rm -rf ~/.config/hypr
+
+cd dotfiles
+
+stow *
+
+cd
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+sudo systemctl enable bluetooth
+
+sudo systemctl start bluetooth
+
+reboot
